5e9f00672ad28a3d433460a527639a4613a82631
Tait Hoyem <tait@tait.tech>
2021-11-13 10:03:23 -0700

    Tree program ready

---
+++ tree/README.md
@@ -0,0 +1,10 @@
+# Trees
+
+To change the output of the tree, read the data.py file.
+
+Install one (optional) package: `bs4` (Beautiful Soup 4), with `pip install -r requirements.txt`.
+This will make the output "pretty" (i.e. indented).
+
+To print the output to terminal, run `python tree.py`
+
+To save to a file, run `python tree.py file.html`.
---
+++ tree/data.py
@@ -0,0 +1,23 @@
+# used if you want to add links to the tree, or a node in the tree
+ID = "t0"
+
+# example of tree (commented out)
+'''
+tree  = [
+  ("root", [
+    ("child", [
+      "grandchild", "grandchild"
+    ]),
+    ("child", []),
+  ]),
+]
+'''
+
+# every node is written ("value", [])
+# the list inside contains all children of that node
+# a string can also be used (i.e. "grandchildren"), but it can have no children
+
+# the tree you want converted to HTML, here:
+tree = [
+  ("root value", [])
+]
